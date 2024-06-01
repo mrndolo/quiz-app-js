3ba7c798e81892dc2994def977e99b80ae22c0d1
mrndolo <callistomutunga@gmail.com>
2024-06-01 09:05:06 +0300

    Added the questions in an array

---
+++ script.js
@@ -0,0 +1,83 @@
+const questions = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answers: [
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Shri Lanka", correct: false},
+        ]
+    },
+    {
+        question: "Which is the largest desert in the world?",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antartica", correct: true},
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
+        ]
+    },
+    {
+        question: "What is the largest river in the world by water volume?",
+        answers: [
+          { text: "Nile", correct: false },
+          { text: "Amazon", correct: true },
+          { text: "Mississippi", correct: false },
+          { text: "Yangtze", correct: false },
+        ]
+    },
+    {
+        question: "What is the largest ocean in the world?",
+        answers: [
+          { text: "Atlantic", correct: false },
+          { text: "Indian", correct: false },
+          { text: "Arctic", correct: false },
+          { text: "Pacific", correct: true },
+        ]
+    },
+    {
+        question: "What is the smallest bone in the human body?",
+        answers: [
+          { text: "Stapes (in the ear)", correct: true },
+          { text: "Phalanges (in fingers)", correct: false },
+          { text: "Coccyx (tailbone)", correct: false },
+          { text: "Hyoid (in throat)", correct: false },
+        ]
+    },
+    {
+        question: "Which of these is NOT a primary color?",
+        answers: [
+          { text: "Red", correct: false },
+          { text: "Blue", correct: false },
+          { text: "Yellow", correct: false },
+          { text: "Green", correct: true },
+        ]
+    },
+    {
+        question: "Which of these animals cannot fly?",
+        answers: [
+          { text: "Bat", correct: false },
+          { text: "Penguin", correct: true },
+          { text: "Eagle", correct: false },
+          { text: "Butterfly", correct: false },
+        ]
+    },
+];

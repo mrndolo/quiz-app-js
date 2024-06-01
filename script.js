142532264d013b53ec94594cd38185dc1d85eb86
mrndolo <callistomutunga@gmail.com>
2024-06-01 09:34:58 +0300

    Displayed questions and answers

--- script.js
+++ script.js
@@ -79,5 +79,45 @@ const questions = [
           { text: "Eagle", correct: false },
           { text: "Butterfly", correct: false },
         ]
-    },
+    }
 ];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    //reset previous question and answer
+    resetState();
+    // code to display questions
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    // code to display answers
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none"
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+startQuiz();
